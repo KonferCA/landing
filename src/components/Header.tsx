385262d7af45640114314ff99380817d18fc6600
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-09-17 04:32:52 -0400

    Add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <header className="w-full flex-none p-6 lg:p-12 text-black">
+            <div className="flex justify-between">
+                <div>
+                    <h1 className="text-5xl text-headingText font-bold"> KONFER INC. </h1>
+                    <span className=""> BRIDGING TALENT & INNOVATION. </span>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export { Header };
