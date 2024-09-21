b559615ae5bb4ec742a43c07ac1696430b082295
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-09-21 10:46:52 -0400

    Wait this tagline sucks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,6 @@ const Header = () => {
             <div className="max-w-12xl mx-auto p-6 lg:p-12 flex justify-center items-center">
                 <div className="text-center">
                     <h1 className="text-3xl lg:text-4xl font-bold tracking-tight hover:text-[#345d95] transition-colors duration-300">KONFER INC.</h1>
-                    <span className="text-sm lg:text-base text-gray-400">BRIDGING TALENT & INNOVATION</span>
                 </div>
             </div>
         </header>
