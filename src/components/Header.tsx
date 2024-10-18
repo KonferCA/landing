ce3d9b27965aef27da995686895bb3ffea6dbbf1
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-10-18 19:22:55 -0400

    Redesign header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,22 @@
 const Header = () => {
     return (
-        <header className="w-full text-white">
-            <div className="max-w-12xl mx-auto p-6 lg:p-12 flex justify-center items-center">
-                <div className="text-center">
-                    <h1 className="text-3xl lg:text-4xl font-bold tracking-tight hover:text-[#345d95] transition-colors duration-300">KONFER INC.</h1>
-                </div>
+        <header className="w-full text-white p-4 pt-10 flex items-center justify-between">
+            <div className="flex items-center pl-12">
+                <img
+                    src="/konfer_logo.svg"
+                    alt="Konfer Logo"
+                    className="h-10 w-auto mr-6"
+                />
+            </div>
+            <nav className="flex-grow flex justify-center">
+                <ul className="flex space-x-6 font-montserrat font-bold text-xl">
+                    <li><a href="#about-us">About Us</a></li>
+                    <li><a href="#our-work">Our Work</a></li>
+                    <li><a href="#partners">Partners</a></li>
+                </ul>
+            </nav>
+            <div className="pr-12">
+                <a href="#contact" className="text-white px-4 py-2 rounded font-montserrat font-bold text-2xl">Contact</a>
             </div>
         </header>
     );
