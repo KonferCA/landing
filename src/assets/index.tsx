e60d2786757298c424cbe76f4576e8f3cef67d7b
jc <46619361+juancwu@users.noreply.github.com>
2024-10-22 11:13:08 -0400

    add headshot photos

---
+++ src/assets/index.tsx
@@ -0,0 +1,23 @@
+import NausherHeadshot from './nausher-headshot.jpg';
+import ArnavHeadshot from './arnav-headshot.jpg';
+import MaxHeadshot from './max-headshot.jpg';
+import NishHeadshot from './nish-headshot.jpg';
+import VincenzoHeadshot from './vincenzo-headshot.jpg';
+import ShakibHeadshot from './shakib-headshot.jpg';
+import AmirHeadshot from './amir-headshot.jpg';
+import JuanHeadshot from './juan-headshot.jpg';
+import AidanHeadshot from './aidan-headshot.jpg';
+import ColmanHeadshot from './colman-headshot.jpg';
+
+export {
+    NausherHeadshot,
+    ArnavHeadshot,
+    MaxHeadshot,
+    NishHeadshot,
+    VincenzoHeadshot,
+    ShakibHeadshot,
+    AmirHeadshot,
+    JuanHeadshot,
+    AidanHeadshot,
+    ColmanHeadshot,
+};
