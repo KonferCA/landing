d8934506c91dc00001cd214af19c6032e7babbe6
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-11-05 20:18:31 -0400

    Update component exports

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 export { Header } from './Header';
 export { About } from './About';
+export { Events } from './Events';
 export { Contact } from './Contact';
 export { Partners } from './Partners';
