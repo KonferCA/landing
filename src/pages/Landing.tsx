9d34337bf5a4004e0bdba7e4648c2f211466d191
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-11-05 20:18:48 -0400

    Add events section

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,10 +1,11 @@
-import { Header, About, Contact, Partners } from '@components';
+import { Header, About, Events, Contact, Partners } from '@components';
 
 const Landing = () => {
     return (
         <main className="min-h-screen bg-[#1C1C1C] text-white">
             <Header />
             <About />
+            <Events />
             <Partners />
             <Contact />
         </main>
