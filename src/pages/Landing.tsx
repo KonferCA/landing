0d5236bbdd543c4cec1c2996c6435feae9a37332
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-11-05 20:38:01 -0400

    Add team section

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,10 +1,11 @@
-import { Header, About, Contact, Partners } from '@components';
+import { Header, About, Contact, Partners, Team } from '@components';
 
 const Landing = () => {
     return (
         <main className="min-h-screen bg-[#1C1C1C] text-white">
             <Header />
             <About />
+            <Team />
             <Partners />
             <Contact />
         </main>
