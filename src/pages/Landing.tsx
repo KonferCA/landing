2d6579a7cb512cd7805fa33947681f631770a54d
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-11-15 00:44:07 -0400

    remove background gradient definition

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,7 +2,7 @@ import { Header, Hero, About, Contact, Partners, Expertise, Events, Team, Footer
 
 const Landing = () => {
     return (
-        <main className="min-h-screen bg-[#1C1C1C] text-white">
+        <main className="min-h-screen text-white">
             <Header />
             <Hero />
             <Expertise />
