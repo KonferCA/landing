242b627cb90fdd30d097f3e0ae0b5247c8a1ea25
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-11-21 06:19:45 -0400

    refactor asset file structure

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,48 +1,76 @@
-import HawkHacksLogo from './hawkhacks-logo.svg';
-import HawkHacksBrand from './hawkhacks-brand.svg';
-import AvalancheBrand from './avalanche-brand.svg';
-import NEARTorontoBrand from './near-toronto-brand.svg';
-import SPURBrand from './spur-brand.svg';
-import LCSLogo from './lcs-logo.svg';
-import LCSLogoEvent from './lcs-logo-nobg.png';
-import SPURLogo from './spur-logo.svg';
-import ETHTorontoLogo from './eth-toronto-logo.svg';
-import ETHWomenLogo from './eth-women-logo.svg';
-import LCSEvent from './lcs-event.png';
-import CollisionEvent from './collision-event.png';
-import CollisionLogo from './collision-logo.png';
-import HawkHacksEvent from './hawkhacks-event.png';
-import footerIcon from './footerIcon.svg';
+// Events
+import LCSLogoEvent from './events/lcs-logo-nobg.png';
+import LCSEvent from './events/lcs-event.png';
+import CollisionEvent from './events/collision-event.png';
+import CollisionLogo from './events/collision-logo.png';
+import HawkHacksEvent from './events/hawkhacks-event.png'
+import HawkHacksLogo from './events/hawkhacks-logo.svg';
+
+// Expertise
+import PartiesImage from './expertise/parties.png';
+import ConferencesImage from './expertise/conferences.png';
+import NetworkingImage from './expertise/networking.png';
+import SeminarsImage from './expertise/seminars.png';
+
+// Hero
+import HeroImage from './hero/hero.png';
+
+// Partners
+import LCSLogo from './partners/lcs-logo.svg';
+import HawkHacksBrand from './partners/hawkhacks-brand.svg';
+import AvalancheBrand from './partners/avalanche-brand.svg';
+import NEARTorontoBrand from './partners/near-toronto-brand.svg';
+import SPURBrand from './partners/spur-brand.svg';
+import SPURLogo from './partners/spur-logo.svg';
+import BlockchainFuturist from './partners/blockchain-futurist-conference-logo.svg'
+import ETHTorontoLogo from './partners/eth-toronto-logo.svg';
+import ETHWomenLogo from './partners/eth-women-logo.svg';
 
 // Team
-import Aidan from './aidan-headshot.jpg';
-import Amir from './amir-headshot.jpg';
-import Arnav from './arnav-headshot.jpg';
-import Colman from './colman-headshot.jpg';
-import Juan from './juan-headshot.jpg';
-import Max from './max-headshot.jpg';
-import Nausher from './nausher-headshot.jpg';
-import Nish from './nish-headshot.jpg';
-import Shakib from './shakib-headshot.jpg';
-import Vincenzo from './vincenzo-headshot.jpg';
+import Aidan from './team/aidan-headshot.jpg';
+import Amir from './team/amir-headshot.jpg';
+import Arnav from './team/arnav-headshot.jpg';
+import Colman from './team/colman-headshot.jpg';
+import Juan from './team/juan-headshot.jpg';
+import Max from './team//max-headshot.jpg';
+import Nausher from './team/nausher-headshot.jpg';
+import Nish from './team/nish-headshot.jpg';
+import Shakib from './team/shakib-headshot.jpg';
+import Vincenzo from './team/vincenzo-headshot.jpg';
+
+// Brand
+import FooterIcon from './brand/footerIcon.svg';
 
 export {
-    HawkHacksLogo,
-    HawkHacksBrand,
-    AvalancheBrand,
-    NEARTorontoBrand,
-    SPURBrand,
+    // Events
     LCSLogo,
-    SPURLogo,
-    ETHTorontoLogo,
-    ETHWomenLogo,
     LCSEvent,
     CollisionEvent,
     CollisionLogo,
     HawkHacksEvent,
     LCSLogoEvent,
-    footerIcon,
 
+    // Expertise
+    PartiesImage,
+    ConferencesImage,
+    NetworkingImage,
+    SeminarsImage,
+
+    // Hero
+    HeroImage,
+
+    // Partners
+    HawkHacksLogo,
+    HawkHacksBrand,
+    AvalancheBrand,
+    NEARTorontoBrand,
+    BlockchainFuturist,
+    SPURBrand,
+    SPURLogo,
+    ETHTorontoLogo,
+    ETHWomenLogo,
+
+    // Team
     Aidan,
     Amir,
     Arnav,
@@ -53,4 +81,7 @@ export {
     Nish,
     Shakib,
     Vincenzo,
+
+    // Brand
+    FooterIcon,
 };
