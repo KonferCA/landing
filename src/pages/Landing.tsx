3bd20918973d1abc80b87cd7e0fe97c86b514a2b
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-11-21 04:50:16 -0400

    update landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,16 +1,25 @@
-import { Header, Hero, About, Contact, Partners, Expertise, Events, Team, Footer } from '@components'
+import { Header, Hero, About, Partners, Expertise, Events, Team, Footer } from '@components'
 
 const Landing = () => {
     return (
         <main className="min-h-screen text-white">
             <Header />
             <Hero />
-            <Expertise />
-            <About />
-            <Events />
-            <Partners />
-            <Contact />
-            <Team />
+            <section className="py-12">
+                <Expertise />
+            </section>
+            <section className="py-12">
+                <Events />
+            </section>
+            <section className="py-8 lg:py-12">
+                <About />
+            </section>
+            <section className="py-2 lg:py-12">
+                <Team />
+            </section>
+            <section className="py-2 lg:py-12">
+                <Partners />
+            </section>
             <Footer />
         </main>
     );
