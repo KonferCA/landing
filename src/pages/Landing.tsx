ee649cb709722b790c933010693d592a2a7d8256
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-11-24 06:30:29 -0400

    add loading screen

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,28 +1,45 @@
-import { Header, Hero, About, Partners, Expertise, Events, Team, Footer } from '@components'
+import { useState, useEffect } from 'react';
+import { Header, Hero, About, Partners, Expertise, Events, Team, Footer, Loading } from '@components';
 
 const Landing = () => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 0);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
-        <main className="min-h-screen text-white">
-            <Header />
-            <Hero />
-            <section className="py-12">
+    <>
+        {loading ? (
+            <Loading />
+            ) : (
+            <main className="min-h-screen text-white">
+                <Header />
+                <Hero />
+                <section className="py-12">
                 <Expertise />
-            </section>
-            <section className="py-12">
+                </section>
+                <section className="py-12">
                 <Events />
-            </section>
-            <section className="py-8 lg:py-12">
+                </section>
+                <section className="py-8 lg:py-12">
                 <About />
-            </section>
-            <section className="py-2 lg:py-8">
+                </section>
+                <section className="py-2 lg:py-8">
                 <Team />
-            </section>
-            <section className="py-2 lg:py-12">
+                </section>
+                <section className="py-2 lg:py-12">
                 <Partners />
-            </section>
-            <Footer />
-        </main>
-    );
+                </section>
+                <Footer />
+            </main>
+        )}
+    </>
+  );
 };
 
 export { Landing };
